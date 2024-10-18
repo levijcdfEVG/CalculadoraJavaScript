0ef2b558a93d58ce7e84d807a13d2ec98934ec6b
Levi Josué Candeias de Figueiredo <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-18 21:34:55 +0100

    Se ha arreglado la logica

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,83 +1,81 @@
 'use strict'
 
-//Coge la cadena del caracter que se ha clicado y lo imprime en el displau
+// Coge la cadena del caracter que se ha clicado y lo imprime en el display
 function addToDisplay(value) {
     document.calc.display.value += value;
 }
 
-//Limpia el display
+// Limpia el display
 function clearDisplay() {
     document.calc.display.value = '';
 }
 
-//Implementa la logica de la calculadora
+// Implementa la logica de la calculadora
 function calculateResult() {
-    let operador = cadenaExtractor(document.calc.display.value); // Extrae el operador
-    let numeroDeOperaciones = contarOperaciones(document.calc.display.value); //Contar el numero de operaciones
-    if (numeroDeOperaciones == 1) {
+    let cadena = document.calc.display.value;
+    let operador = cadenaExtractor(cadena); // Extrae el operador
+    let numeroDeOperaciones = contarOperaciones(cadena); // Contar el numero de operaciones
+    
+    if (numeroDeOperaciones === 1) { // Solo una operación permitida
         switch (operador) {
             case 'suma':
-                sumar(document.calc.display.value); // Llama a la función de suma
+                sumar(cadena); // Llama a la función de suma
                 break;
             case 'resta':
-                restar(document.calc.display.value); // Llama a la función de resta
+                restar(cadena); // Llama a la función de resta
                 break;
             case 'multiplicacion':
-                multiplicar(document.calc.display.value); // Llama a la función de multiplicación
+                multiplicar(cadena); // Llama a la función de multiplicación
                 break;
             case 'division':
-                dividir(document.calc.display.value); // Llama a la función de división
+                dividir(cadena); // Llama a la función de división
                 break;
             default:
                 document.calc.display.value = 'Cálculo no válido'; // Mensaje para cálculo no válido
                 break;
         }
-    }else{
-        document.calc.display.value = 'ERROR: Introduce solo operaciones unicas'
+    } else {
+        document.calc.display.value = 'ERROR: Introduce solo operaciones únicas';
     }
 }
 
-//Analiza la cadena para buscar el operador
-function cadenaExtractor(cadenaDisplay){
+// Analiza la cadena para buscar el operador
+function cadenaExtractor(cadenaDisplay) {
     let operador = null;
-    
     for (let index = 0; index < cadenaDisplay.length; index++) {
         if (cadenaDisplay.charAt(index) == '+') {
             operador = 'suma';
             break;
-        }else if (cadenaDisplay.charAt(index) == '-') {
+        } else if (cadenaDisplay.charAt(index) == '-') {
             operador = 'resta';
             break;
-        }else if (cadenaDisplay.charAt(index) == '*') {
+        } else if (cadenaDisplay.charAt(index) == '*') {
             operador = 'multiplicacion';
             break;
-        }else if (cadenaDisplay.charAt(index) == '/') {
+        } else if (cadenaDisplay.charAt(index) == '/') {
             operador = 'division';
             break;
         }
     }
-
     return operador;
 }
 
-//Busca todas las operaciones que contiene la cadena
-function contarOperaciones(cadenaDisplay){
+// Busca todas las operaciones que contiene la cadena
+function contarOperaciones(cadenaDisplay) {
     let nOperaciones = 0;
     for (let index = 0; index < cadenaDisplay.length; index++) {
         if (cadenaDisplay.charAt(index) == '+' || 
             cadenaDisplay.charAt(index) == '-' || 
             cadenaDisplay.charAt(index) == '*' || 
             cadenaDisplay.charAt(index) == '/') {
-            nOperaciones++; //Incrementa el contador
+            nOperaciones++; // Incrementa el contador
         }
-
     }
-
     return nOperaciones;
 }
 
-//Funciones de Operaciones
-function sumar(cadenaDisplay){
+// Funciones de Operaciones
+function sumar(cadenaDisplay) {
     let numeros = cadenaDisplay.split('+');
     let numero1 = parseFloat(numeros[0]);
     let numero2 = parseFloat(numeros[1]);
